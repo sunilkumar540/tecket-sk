cca77ef4c71b81f837fabf9c6da66807419d4438
sunil <skpal993122@gmail.com>
2024-06-01 18:11:41 +0530

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const student1 ={
+    name:"raju",
+    age:20,
+    rollno: 90,
+  };
+    const student2={
+      name:"raju",
+      age:40,
+      rollno:80  
+    
+  };
+  if(student1.name=== student2.name){
+    console.log("hello world");
+  }else{
+    console.log("unknown");
+  }
+const fun1= ()=>{
+  let num1=9
+  let num2=10
+  let num3=10+9
+  console.log(num3)
+};
+if(student1.name===student2.name){
+  fun1();
+}
+
   return (
     <div className="App">
       <header className="App-header">
